d36c0886bf80b2c3cb8fa24d9b8a6af0153d6ed6
Ali Esmaili <AESM85@gmail.com>
2015-05-10 03:06:50 -0700

    Create test for destroy_spec.rb

---
+++ spec/features/TaskLists/destroy_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "deleting task lists" do
+  let!(:task_list) { TaskList.create(title: "Party!", description: "The list of parties I must attend.") }
+
+  it "successfully destroies the task list" do
+
+    visit "/task_lists"
+
+    within "#task_list_#{task_list.id}" do
+      click_link "Destroy"
+    end
+
+    expect(page).to_not have_content(task_list.title)
+    expect(TaskList.count).to eq(0)
+  end
+
+end
